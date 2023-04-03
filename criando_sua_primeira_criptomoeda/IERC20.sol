d3f7d426433be7cee714206cbc61623bb1d88b27
Dione-b <dionenassau321@gmail.com>
2023-04-03 13:38:54 -0300

    Desafio criando uma criptomoeda

---
+++ criando_sua_primeira_criptomoeda/IERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.18;
+
+interface IERC20 {
+    
+     //getters
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    //functions
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256);
+
+}
